7fed4d3b4e9d00a090568ff9755e35f5bd412cf5
Pareyor <pablo.reyortiz@alumnos.uneatlantico.es>
2025-04-06 16:06:30 +0200

    Actualizacion index

---
+++ config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/tareas_habitos_pro', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ Conectado a MongoDB'))
+.catch((err) => console.error('❌ Error conectando a MongoDB:', err));
+
+module.exports = mongoose;
--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 
 app.use(express.json());
+
+// Ruta de prueba para '/'
+app.get('/', (req, res) => {
+  res.send('¡Hola desde Express + MongoDB!');
+});
+
+// Ruta para '/tasks'
 app.use('/tasks', taskRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en el puerto 3000');
 });
+
